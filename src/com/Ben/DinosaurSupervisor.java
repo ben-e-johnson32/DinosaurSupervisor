7de9db5671dcfe1d19091a4bffccaf3ab8acb135
ben <ben@ben.ben>
2016-09-20 11:37:45 -0500

    Changed 'velociraptor' to 'raptor'

--- src/com/Ben/DinosaurSupervisor.java
+++ src/com/Ben/DinosaurSupervisor.java
@@ -5,10 +5,10 @@ public class DinosaurSupervisor
     public static void main(String[] args)
     {
         Dinosaur t_rex = new Dinosaur();
-        Dinosaur velociraptor = new Dinosaur();
+        Dinosaur raptor = new Dinosaur();
 
         t_rex.roar();
-        velociraptor.roar();
+        raptor.roar();
         System.out.println();
     }
 }
